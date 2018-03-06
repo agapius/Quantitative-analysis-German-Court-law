ff12f7405c7ade4dbcd7f7ebfd71e967747c73d1
Jasper Thinkpad <jasper.castell@campus.tu-berlin.de>
2018-03-06 18:52:57 +0100

    little update with more stuff to do

--- download_bgh.py
+++ download_bgh.py
@@ -1,18 +1,33 @@
 # import libraries
 import urllib2
+from bs4 import BeautifulSoup
+import re
 
+##For all overview Pages (Year, Month)
+#for loop through every year/month
+
+##Specific overview page 
+#for loop through all pages of the current year (pages begin with 0)
+
+#get senat, date, AZ and link to the bgh_urteil_link (pdf link can be extracted from there)
 bgh_urteil_link= "https://juris.bundesgerichtshof.de/cgi-bin/rechtsprechung/document.py?Gericht=bgh&Art=en&Datum=Aktuell&nr=81405&pos=11&anz=516"
 
 p = urllib2.urlopen(bgh_urteil_link)
+soup = BeautifulSoup(p, 'html.parser')
+#gets title tag
+name_box = soup.find("title")
+#gets only the text from the title tag
+clean_name_urteil= name_box.get_text(strip=True)
+#prints the name of the Urteil
+print(clean_name_urteil)
 
 
+
+##Download the Urteil and save it with the appropriate name
 #create a variable with the link to the urteil
 bgh_pdf = "https://juris.bundesgerichtshof.de/cgi-bin/rechtsprechung/document.py?Gericht=bgh&Art=en&Datum=Aktuell&nr=81405&anz=516&pos=11&Frame=4&.pdf"
 
-# query the website and return the html to the variable 
-
-
-
+#f=open link to download
 f = urllib2.urlopen(bgh_pdf)
-with open("Urteil.pdf", "wb") as code:
+with open("%s.pdf" %clean_name_urteil, "wb") as code:
 	code.write(f.read())
