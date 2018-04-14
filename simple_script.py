f2b74957945566abf2916c589204bc812ac10e3f
Jasper <36561872+agapius@users.noreply.github.com>
2018-04-14 11:35:24 +0200

    Works to scrape all RSI - StackQuestion

--- simple_script.py
+++ simple_script.py
@@ -1,21 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
-
 xml = requests.get('https://www.rechtsprechung-im-internet.de/rii-toc.xml')
-
 soup = BeautifulSoup(xml.text, 'xml')
-
-failed_downloads= []
+counter=0
 
 for item in soup.find_all('item'):
-	current_item = item
-	zip_link = current_item.link.text
-	name1 = current_item.gericht.text
-	name2 = current_item.aktenzeichen.text
-	name_final = name1+'_'+name2
-
-	print(zip_link)
-	print(type(zip_link))
-	print(name_final)
-
-print("We found %d Urteile im XML Format" %(len(soup.find_all('link')))
+	ziplink=str(item.link.text) 
+	datum=str(item.find('entsch-datum').text)
+	az=str(item.aktenzeichen.text)
+	hof=str(item.gericht.text)
+	if ziplink.find('/'):
+		name = ziplink.rsplit('/',1)[1]
+	r=requests.get(ziplink, allow_redirects=True)
+	with open('%s' % name,'wb') as f:
+		f.write(r.content)
+	print(name)
+	counter=counter+1
+	print(counter)
