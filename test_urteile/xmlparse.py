64f22e7687e984645491ffd028da87c797c4a562
Jasper Thinkpad <jasper.castell@campus.tu-berlin.de>
2018-05-06 19:08:19 +0200

    updated code works now

--- test_urteile/xmlparse.py
+++ test_urteile/xmlparse.py
@@ -1,19 +1,22 @@
 #Analytics
-from os import listdir
+import os
 import csv
 import re
 import xml.etree.ElementTree as ET
 
 #create a list of all files in directory
-xmllist= os.listdir()
-
-with open('megasheet.csv', 'wb') as csvfile:
+xmllist= []
+for file in os.listdir():
+    if file.endswith(".xml"):
+        xmllist.append(file)
+print(xmllist)
+with open('megasheet.csv', 'w') as csvfile:
 	writer = csv.writer(csvfile, delimiter=',')
 	writer.writerow(['Doknummer', 'Aktenzeichen','Gerichtstyp', 'Spruchkörper', 'Entscheidungsdatum', 'Zitat(Kommentar)', 'Zitat(Entscheidung)', 'Zitat(Autor)'])
 
 	for xml in xmllist:
 		#get data from xml
-		tree = ET.parse('xml')
+		tree = ET.parse(xml)
 		root = tree.getroot()
 
 		##get simple tags
@@ -23,22 +26,5 @@ with open('megasheet.csv', 'wb') as csvfile:
 		entschdatum= root.find('entsch-datum').text
 		aktenzeichen= root.find('aktenzeichen').text
 		doktyp= root.find('doktyp').text
-
-		'''##gets all the text from the decision
-								for p in root.iter('p'):
-									if bool(p.text):
-									###scan the paragraph with re for possible citations
-									###add citations with doknrlist.append(citations)
-									print(p.text)'''
-
-
-
-'''pattern = "\(.*?(NJW).*?\)
-
-with 
-for match in re.findall(pattern, text):'''
-
-
-
 		writer.writerow([doknr, aktenzeichen, gertyp, spruchkoerper, entschdatum, 'Zitat(Kommentar)', 'Zitat(Entscheidung)', 'Zitat(Autor)'])
 		
