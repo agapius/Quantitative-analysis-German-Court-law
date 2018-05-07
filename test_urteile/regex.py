e2429b67bf81435749cadd08f801705fe1474575
d-e-h-i-o <niklas.dehio@gmail.com>
2018-05-07 21:05:42 +0200

    xmlparse will now write a file that shows all citations, citations.txt, from that we can derive often used citations

--- test_urteile/regex.py
+++ test_urteile/regex.py
@@ -1,46 +1,50 @@
 import re
 
+# returns a list of mostly citations
 def clean_list(list):
-	clean_list = []
-	for item in list:
-		if(test_of_tests(item)):
-			clean_list.append(item.replace('vgl. ', ''))
-	
-	return clean_list
+    clean_list = []
+    for item in list:
+        if(test_of_tests(item)):
+            clean_list.append(item.replace('vgl. ', ''))
+    
+    return clean_list
 
+# returns true if string is an article-citation
 def test_for_article(string):
-	return bool(re.search('^§', string))
+    return bool(re.search('^§', string))
 
+# returns true if string has numbers (citations will have numbers), 
+# currently not used, but included in test for citations
 def test_for_has_numbers(string):
-	return bool(re.search(r'\d', string))
+    return bool(re.search(r'\d', string))
 
-
+# returns true is string 
 def test_for_citation(string):
-	if(len(string) <= 3):
-		return False
-	else:
-		return bool(re.search(r'\d', string))
+    if(len(string) <= 3):
+        return False
+    else:
+        return bool(re.search(r'\d', string))
 
 def test_of_tests(string):
-	if(test_for_citation(string) and not test_for_article(string) and test_for_has_numbers(string)):
-		return True
-	else:
-		return False
+    if(test_for_citation(string) and not test_for_article(string) and test_for_has_numbers(string)):
+        return True
+    else:
+        return False
 
 # returns a list of mostly citations
 def scan_paragraph(string):
-	pattern_brackets = '\((.*?)\)'
-	regex_brackets = re.compile(pattern_brackets)
-	list_of_brackets = re.findall(regex_brackets, string)
-	list_of_citations = []
-	for string in list_of_brackets:
-	if(';' in string):
-		split = string.split(';')
-		list_of_citations = list_of_citations + split
-	else:
-		list_of_citations.append(string)
-	list_of_citations = clean_list(list_of_citations)
-	return list_of_citations
+    pattern_brackets = '\((.*?)\)'
+    regex_brackets = re.compile(pattern_brackets)
+    list_of_brackets = re.findall(regex_brackets, string)
+    list_of_citations = []
+    for string in list_of_brackets:
+        if(';' in string):
+            split = string.split(';')
+            list_of_citations = list_of_citations + split
+        else:
+            list_of_citations.append(string)
+    list_of_citations = clean_list(list_of_citations)
+    return list_of_citations
 
 
 # KORE303862016.xml
@@ -58,22 +62,22 @@ regex_brackets = re.compile(pattern_brackets)
 list_of_brackets = re.findall(regex_brackets, string)
 
 for string in list_of_brackets:
-	print(string)
+    print(string)
 
 list_of_citations = []
 
 for string in list_of_brackets:
-	if(';' in string):
-		split = string.split(';')
-		list_of_citations = list_of_citations + split
-	else:
-		list_of_citations.append(string)
+    if(';' in string):
+        split = string.split(';')
+        list_of_citations = list_of_citations + split
+    else:
+        list_of_citations.append(string)
 
 
 list_of_citations = clean_list(list_of_citations)
 
 print('CLEAN')
 for string in list_of_citations:
-	print(string)'''
+    print(string)'''
 
 
--- test_urteile/xmlparse.py
+++ test_urteile/xmlparse.py
@@ -11,31 +11,36 @@ for file in os.listdir():
     if file.endswith(".xml"):
         xmllist.append(file)
 
+'''
 #open the megasheet.csv and create the headers
 with open('megasheet.csv', 'w') as csvfile:
 	writer = csv.writer(csvfile, delimiter=',')
 	writer.writerow(['Doknummer', 'Aktenzeichen','Gerichtstyp', 'Spruchkörper', 'Entscheidungsdatum', 'Zitat(Kommentar)', 'Zitat(Entscheidung)', 'Zitat(Autor)'])
+'''
+file = open('citations.txt', 'r+')
 
-
-	##loop through all xml files in the diectory
-	for xml in xmllist:
-		#get data from xml general
-		tree = ET.parse(xml)
-		root = tree.getroot()
-		##get specific data tags
-		doknr= root.find('doknr').text
-		gertyp= root.find('gertyp').text
-		spruchkoerper= root.find('spruchkoerper').text
-		entschdatum= root.find('entsch-datum').text
-		aktenzeichen= root.find('aktenzeichen').text
-		doktyp= root.find('doktyp').text
-		##get the citations 
-		for p in root.iter('p'):
-			if bool(p.text):
-			###scan the paragraph with re for possible citations
-			doknrlist = doknrlist + scan_paragraph(p.text)
-			###add citations with doknrlist.append(citations)
-			print(p.text)
-
-		writer.writerow([doknr, aktenzeichen, gertyp, spruchkoerper, entschdatum, 'Zitat(Kommentar)', 'Zitat(Entscheidung)', 'Zitat(Autor)'])
-		
+##loop through all xml files in the diectory
+for xml in xmllist:
+	#get data from xml general
+	tree = ET.parse(xml)
+	root = tree.getroot()
+	##get specific data tags
+	doknr= root.find('doknr').text
+	gertyp= root.find('gertyp').text
+	spruchkoerper= root.find('spruchkoerper').text
+	entschdatum= root.find('entsch-datum').text
+	aktenzeichen= root.find('aktenzeichen').text
+	doktyp= root.find('doktyp').text
+	##get the citations 
+	for p in root.iter('p'):
+		if bool(p.text):
+		###scan the paragraph with re for possible citations
+			citation_list = scan_paragraph(p.text)
+			for item in citation_list:
+				file.write(item)
+				file.write('\n')
+		###add citations with doknrlist.append(citations)		
+file.close()
+print('Done')
+#	writer.writerow([doknr, aktenzeichen, gertyp, spruchkoerper, entschdatum, 'Zitat(Kommentar)', 'Zitat(Entscheidung)', 'Zitat(Autor)'])
+	
