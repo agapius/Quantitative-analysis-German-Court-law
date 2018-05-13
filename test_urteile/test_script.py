33658d257c9029a56341d85446d75b50b697afd7
Jasper Thinkpad <jasper.castell@campus.tu-berlin.de>
2018-05-13 19:23:43 +0200

    added statistics to the test script so one can see how many citation the regex captured!

--- test_urteile/test_script.py
+++ test_urteile/test_script.py
@@ -5,17 +5,23 @@ import sys
 ## a file with the name of the script and everythin else in citations_reduced. 
 ## After first use the open will should also be citations_reduced
 
+output_counter = 0
 regular_expression = sys.argv[1]
 ergebnisse = open(regular_expression, 'w')
 
 with open('citations_reduced.txt', 'r') as input:
 	lines = input.readlines()
+	input_counter = (len(lines))
 	input.close()
 
-with open('citations_reduced.txt', 'w') as output:
+with open('citations_reduced.txt', 'w+') as output:
 	for line in lines:
 		if(re.search(regular_expression, line)):
 			ergebnisse.write(line)
 		else:
 			output.write(line)
+			output_counter= output_counter +1
 
+print('\n')
+print(regular_expression)
+print('captured %d citations.' %(input_counter - output_counter))
