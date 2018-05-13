699e6b7d02540977b7a6233e93eea244a987edb1
Jasper Thinkpad <jasper.castell@campus.tu-berlin.de>
2018-05-13 17:50:16 +0200

    aggregates all Regex. Goes through cittions.txt. Checks how many citations are captured.

--- test_urteile/test_script_all.py
+++ test_urteile/test_script_all.py
@@ -9,52 +9,55 @@ After first use the open will should also be citations_reduced'''
 ########################### REggexes ##########################################
 regexes_dict={
 'Meyer-Goßner' 		: '(Meyer-Goßner)',
-'MüKo-BGB'			: '(MüKo BGB),(MüKoBGB),(MüKo-BGB),[MünchKommBGB),[MünchKomm-BGB)',
-'MüKo-ZPO'			: '(MüKoZPO),(MüKo-ZPO),(MünchKommZPO),(MünchKomm-ZPO),(MünchKomm ZPO)]',
-'MüKo-GmbHG'		: '(MünchKommGmbHG),(MüKo-GmbHG),(MüKoGmbHG),(MünchKomm-GmbHG)',
-'MüKo-InsO'			: '(MüKoInsO),(MüKo-InsO),(MünchKommInsO),(MünchKomm-InsO)',
+'MüKo-BGB'			: '(MüKo BGB)|(MüKoBGB)|(MüKo-BGB)|(MünchKommBGB)|(MünchKomm-BGB)',
+'MüKo-ZPO'			: '(MüKoZPO)|(MüKo-ZPO)|(MünchKommZPO)|(MünchKomm-ZPO)|(MünchKomm ZPO)]',
+'MüKo-GmbHG'		: '(MünchKommGmbHG)|(MüKo-GmbHG)|(MüKoGmbHG)|(MünchKomm-GmbHG)',
+'MüKo-InsO'			: '(MüKoInsO)|(MüKo-InsO)|(MünchKommInsO)|(MünchKomm-InsO)',
 'Schönke/Schröder'	: '(Schönke/Schröder)', 
 'Sodan/Ziekow'		: '(Sodan+/Ziekow)',
 'Staudinger'		: '(Staudinger)',
 'Palandt'			: '(Palandt)',
-'BeckOK-BGB'		: '(Beck-OK-BGB),(BeckOK-BGB),(Beck OK BGB),(BeckOK BGB)',
-'BeckOK-ZPO'		: '(BeckOK-ZPO),(BeckOK ZPO),(Beck OK ZPO)',
+'BeckOK-BGB'		: '(Beck-OK-BGB)|(BeckOK-BGB)|(Beck OK BGB)|(BeckOK BGB)',
+'BeckOK-ZPO'		: '(BeckOK-ZPO)|(BeckOK ZPO)|(Beck OK ZPO)',
 }
 
 
-print(regexes_dict.items())
-
-'''############################# START ###########################################
+############################# START ###########################################
 
 #Counter for the lines (# of citations) in citations.txt
 input_line_count=0
 #Counter for the lines in output file
 output_line_count=0
 
-regular_expression = sys.argv[1]
-#List of all captured citations
-ergebnisse = []
 
-
-
-with open('citations.txt', 'r') as citations:
-	with open('citations_reduced.txt', 'r+') as citations_reduced:
-		for line in citations:
-			input_line_count = input_line_count+1
-			if(re.search(regular_expression, line)):
-				ergebnisse.append(line)
-			else:
-				citations_reduced.write(line)
-				output_line_count = output_line_count+1
-
+for key, value in regexes_dict.items():
+	regular_expression = value
+	#List of all captured citations
+	ergebnisse = []
+	
+	
+	
+	with open('citations.txt', 'r') as citations:
+		with open('citations_reduced.txt', 'r+') as citations_reduced:
+			for line in citations:
+				input_line_count = input_line_count+1
+				if(re.search(regular_expression, line)):
+					ergebnisse.append(line)
+				else:
+					citations_reduced.write(line)
+					output_line_count = output_line_count
+		print('%s captured:' %(regular_expression))
+		print('%d lines' %(len(ergebnisse)))
+		citations=citations_reduced
 
 ##Output list of all captured citations
-for citation in ergebnisse:
+'''for citation in ergebnisse:
 	print(citation)
-print('\n')
+print('\n')'''
 
 ##Output length of input and output
+print('\n')
 print('The input file was %d lines long' %(input_line_count)) 
 print('The output file is only %d lines long' %(output_line_count))
 print('Congratulations, you captured %d lines' %(len(ergebnisse)))
-print('\n')'''
+print('\n')
