76c3c78315b14573f96d679541ab4dcf7c1ef9d5
d-e-h-i-o <niklas.dehio@gmail.com>
2018-05-16 18:12:49 +0200

    many changes, alpha version is in near reach

--- test_urteile/regex.py
+++ test_urteile/regex.py
@@ -40,6 +40,9 @@ def scan_paragraph(string):
     for string in list_of_brackets:
         if(';' in string):
             split = string.split(';')
+            split = split.split('und')
+            split = split.split('bestätigt')
+            split = split.split('Hinweis')
             list_of_citations = list_of_citations + split
         else:
             list_of_citations.append(string)
@@ -49,13 +52,96 @@ def scan_paragraph(string):
 # returns source, loops trough dictionary, values are RegExs 
 def find_source(string):
     
-    dictionary = {#'NJW': '(NJW)', #also matches NJW-RR PROBLEM: URTEIL VON AUFSATZ UNTERSCHEIDEN
-    'MüKo-BGB': '(MüKoBGB)|(MüKo-BGB)|(MünchKommBGB)',
-    'Sodan/Ziekow': '(Sodan+/Ziekow)'
-    #'Meyer-Goßner': '',
-    #'Schönke/Schröder': '', 
+    dictionary = {
+    'BGH':              '((I |II |III |IV |V |VI |VII |VIII |IX |X |XI |XII |XIV )(ZR|ZA|ZB|ZR(Ü)|BLw|LwZR|LwZA|LwZB|GSZ|AR(VZ)|ARZ|StR|StB|GSSt|AR(VS)|AR(Vollz)|ARs|ARP|BJs|StE|BGs|AK|BGns|B|(VGS|VRG)|AR|AnwZ|AnwZ(P)|AnwZ(B)|AnwZ(Brfg)|AnwSt|AnwSt(R|B)|KZR|KZA|KZB|KVR|KVZ|KRB|AR(Kart)|EnZR|EnZA|EnZB|EnVR|EnVZ|EnRB|AR(Enw)|NotZ|NotZ(Brfg)|NotSt(B)|NotSt(Brfg)|PatAnwZ|PatAnwSt(R|B)|RiZ|RiZ(R|B)|RiSt|RiSt(R|B)|AR(Ri)|StbSt(R|B)|WpSt(R|B)))|(BGH)',
 
-    }
+    'BVerfG':           '(BVerfG)|(Bundesverfassungsgericht)',
+
+    'LG Hamburg':       '(LG Hamburg)|(Landgericht(s)? Hamburg)', 
+    'AG Hamburg':       '(AG Hamburg)',
+    'Staudinger':       '(Staudinger)',
+    'OLG Hamm' :        '(OLG|Olg).*(Hamm)',
+    'OLG München' :     '((OLG|Olg).*(München))|(BayObLG)',
+    'OLG Stuttgart' :   '(OLG|Olg).*(Stuttgart)',
+    'OLG Frankfurt' :   '(OLG|Olg).*(Frankfurt)',
+    'OLG Düsseldorf' :  '(OLG|Olg).*(Düsseldorf)',
+    'OLG Karlsruhe' :   '(OLG|Olg).*(Karlsruhe)',
+    'OLG Dresden' :     '(OLG|Olg).*(Dresden)',
+    'OLG Köln' :        '(OLG|Olg).*(Köln)',
+    'OLG Celle' :       '(OLG|Olg).*(Celle)',
+    'OLG Berlin' :      '(OLG|Olg).*(Berlin)',
+    'OLG Nürnberg' :    '(OLG|Olg).*(Nürnberg)',
+    'OLG Schleswig' :   '(OLG|Olg).*(Schleswig)',
+    'OLG Koblenz' :     '(OLG|Olg).*(Koblenz)',
+    'OLG Brandenburg' : '(OLG|Olg).*(Brandenburg)|Brandenburgisches OLG',
+    'OLG Oldenburg' :   '(OLG|Olg).*(Oldenburg)',
+    'OLG Naumburg' :    '(OLG|Olg).*(Naumburg)|OLG (des Landes)?(?=.*Sachsen-Anhalt)',
+    'OLG Bamberg' :     '(OLG|Olg).*(Bamberg)',
+    'OLG Jena' :        '(OLG|Olg).*(Jena)',
+    'OLG Hamburg' :     '(OLG|Olg).*(Hamburg)|Hanseatisches Oberlandesgericht Hamburg' ,
+    'OLG Rostock' :     '(OLG|Olg).*(Rostock)',
+    'OLG Zweibrücken' : '(OLG|Olg).*(Zweibrücken)',
+    'OLG Braunschweig': '(OLG|Olg).*(Braunschweig)',
+    'OLG Saarbrücken':  '(OLG|Olg).*(Saarbrücken)',
+    'OLG Bremen' :      '(OLG|Olg).*(Bremen)',
+    'KG':               '^ ?KG[, ]|KG(?=.*Berlin)|Kammergericht|(?!=Co.) KG, ',
+    'OLG Saarland':     'Saarländisches OLG|OLG Saarland',
+
+    
+    'MüKo-BGB': '(MüKo BGB)|(Mü[kK]o-?BGB)|(MünchKomm.?BGB)|(MünchKomm-BGB)|(MüKomm-BGB)|MünchKomm(?=.*BGB)|MüKo(?=.*BGB)|Münchener Kommentar zum BGB',
+    'MüKo-ZPO': '(MüKo[ -/]?ZPO)|(MünchKomm.?ZPO)|(MünchKomm-ZPO)|(MünchKomm[ -/.]?ZPO)|MünchKomm(?=.*ZPO)',
+    'MüKo-GmbHG': '(MünchKommGmbHG)|(MüKo-?GmbHG)|(MünchKomm-GmbHG)',
+    'MüKo-InsO' : '(MüKo[ -]?InsO)|(MünchKomm[ -]?InsO)',
+    'MüKo-StGB': 'Mü[kK]o[ -/]?StGB|MünchKomm[ -]?StGB|MüKomm-StGB',
+    'MüKo-StPO': 'MüKo[ -]?StPO|MünchKomm[ -]?StPO',
+    'MüKo-HGB'  : '(MüKo[ -.]?HGB)|(MünchKomm[ -.]?HGB)',
+    'MüKo-AktG' : '(Mü[kK]o[ -.]?AktG)|(MünchKomm[ -.]?AktG)',
+    
+   
+    'Sodan/Ziekow'      : '(Sodan+/Ziekow)', 
+    
+    'Staudinger': '(Staudinger)',
+    'Palandt'       : '(Palandt)',
+    'jurisPK-BGB': 'jurisPK[ -/]?BGB|juris Praxiskommentar BGB',
+
+    'BeckOK-BGB'        : '(Beck[ -]?OK[ -]?BGB)',
+    'BeckOK-ZPO'        : '(Beck[ -]?OK[ -]?ZPO)|Vorwerk(?=.*ZPO)|Beck\'scher Online-Kommentar ZPO',
+    'BeckOK-StGB' : '(Beck[ -]?OK[ -]?StGB)',
+    'BeckOK-Datenschutzrecht': '(Beck[ -]?OK[ -]?Datenschutzrecht)',
+    'BeckOK-StPO'       : '(Beck[ -]?OK[ -]?StPO)',
+    'BeckOK-BauGB'      : '(Beck[ -]?OK[ -]?BauGB)',
+    'BeckOK-FamFG'      : '(Beck[ -]?OK[ -]?FamFG)',
+    'BeckOK-HGB'        : '(Beck[ -]?OK[ -]?HGB)',
+    'BeckOK-WEG'        : '(Beck[ -]?OK[ -]?WEG)',
+    'BeckOK-UrhR'       : '(Beck[ -]?OK[ -]?UrhR)',
+    'BeckOK-AuslR'      : '(Beck[ -]?OK[ -]?AuslR)',
+    'BeckOK-GG'     : '(Beck[ -]?OK[ -]?GG)',
+    'BeckOK-VVG'        : '(Beck[ -]?OK[ -]?VVG)',
+    'BeckOK-GBO'        : '(Beck[ -]?OK[ -]?GBO)',
+    'BeckOK-VwVfG'      : '(Beck[ -]?OK[ -]?VwVfG)',
+
+    
+    'Meyer-Goßner': '^(?!.*Festschrift.*Meyer-Goßner).*(Meyer-Goßner)',
+    'Fischer': 'Fischer[ -/,]?[ ]?StGB|(Fischer, aaO)',
+    'Leipziger Kommentar': 'Leipziger Kommentar',
+    'Schönke/Schröder'  : '(Schönke/Schröder)', 
+
+    'Thomas/Putzo': 'Thomas/Putzo',
+    'Musielak-ZPO': 'Musielak(?=.*(ZPO)|.*(aaO))',
+    'Zöller-ZPO': 'Zöller(?=.*ZPO)|Zöller/Heßler|Zöller/Vollkommer|Zöller/Stöber|Zöller/Greger|Zöller/Geimer|Zöller/Philippi',
+    'Baumbach-ZPO': 'Baumbach(?=.*ZPO)|Baumbach/(Lauterbach/)?(Albers/)?Hartmann',
+    'Wieczorek-ZPO': 'Wieczorek(?=/Schütze|.*ZPO)',
+    'Prütting-ZPO': 'Prütting(?=/Gehrlein|.*ZPO)',
+    'Hk-ZPO': 'H[kK][ -]?ZPO',
+    'Stein/Jonas-ZPO': 'Stein(?=/Jonas|.*ZPO)',
+    'Saenger-ZPO': 'Saenger(?=.*ZPO)|Saenger/Saenger',
+    'Zimmermann-ZPO': 'Zimmermann(?=.*ZPO)',
+    
+    'Musielak-FamG': 'Musielak(?=.*FamFG)',
+    'Baumbach-GmbH': 'Baumbach(?=.*GmbH|/Hueck)',
+    'Baumbach-HGB': 'Baumbach(?=.*HGB|/Hopt)',
+    'Hk-InsO': 'H[kK][ -]?InsO',
+}
     
     for key, value in dictionary.items():
         if(re.search(value, string)):
--- test_urteile/xmlparse.py
+++ test_urteile/xmlparse.py
@@ -15,9 +15,21 @@ for file in os.listdir():
 #open the megasheet.csv and create the headers
 with open('megasheet.csv', 'w') as csvfile:
 	writer = csv.writer(csvfile, delimiter=',')
-	writer.writerow(['Doknummer', 'Aktenzeichen','Gerichtstyp', 'Spruchkörper', 'Entscheidungsdatum', 'Zitat(Kommentar)', 'Zitat(Entscheidung)', 'Zitat(Autor)'])
+	writer.writerow(['Doknummer', 'Aktenzeichen','Gerichtstyp', 'Spruchkörper', 'Entscheidungsdatum', 'Jahr', 
+		'BGH', 'BVerfG', 'LG Hamburg', 'AG Hamburg', 'OLG Hamm', 'OLG München', 'OLG Stuttgart',
+		'OLG Frankfurt', 'OLG Düsseldorf', 'OLG Karlsruhe', 'OLG Dresden', 'OLG Köln', 'OLG Celle',
+		'OLG Berlin', 'OLG Nürnberg', 'OLG Schleswig', 'OLG Koblenz', 'OLG Brandenburg', 'OLG Oldenburg',
+		'OLG Naumburg', 'OLG Bamberg', 'OLG Jena', 'OLG Hamburg', 'OLG Rostock', 'OLG Zweibrücken',
+		'OLG Braunschweig', 'OLG Saarbrücken', 'OLG Bremen', 'KG', 'OLG Saarland', 'MüKo-BGB',
+		'MüKo-ZPO', 'MüKo-GmbHG', 'MüKo-InsO', 'MüKo-StGB', 'MüKo-StPO', 'MüKo-HGB', 'MüKo-AktG',
+		'Sodan/Ziekow', 'Staudinger', 'Palandt', 'jurisPK-BGB', 'BeckOK-BGB', 'BeckOK-ZPO', 'BeckOK-StGB',
+		'BeckOK-Datenschutzrecht', 'BeckOK-StPO', 'BeckOK-BauGB', 'BeckOK-FamFG', 'BeckOK-HGB', 'BeckOK-WEG',
+		'BeckOK-UrhR', 'BeckOK-AuslR', 'BeckOK-GG', 'BeckOK-VVG', 'BeckOK-GBO', 'BeckOK-VwVfG',
+		'Meyer-Goßner', 'Fischer', 'Leipziger Kommentar', 'Schönke/Schröder', 'Thomas/Putzo', 'Musielak-ZPO'
+		'Zöller-ZPO', 'Baumbach-ZPO', 'Wieczorek-ZPO', 'Prütting-ZPO', 'Hk-ZPO', 'Stein/Jonas-ZPO',
+		'Saenger-ZPO', 'Zimmermann-ZPO', 'Musielak-FamG', 'Baumbach-GmbH', 'Baumbach-HGB', 'Hk-InsO'])
 
-file = open('citations.txt', 'r+')
+#file = open('citations.txt', 'r+')
 
 # we need the length to calculate the percentage done; same goes for counter 
 length = len(xmllist)
@@ -32,6 +44,7 @@ for xml in xmllist:
 	gertyp= root.find('gertyp').text
 	spruchkoerper= root.find('spruchkoerper').text
 	entschdatum= root.find('entsch-datum').text
+	jahr= int(str(entschdatum)[0:4])
 	aktenzeichen= root.find('aktenzeichen').text
 	doktyp= root.find('doktyp').text
 	##get the citations 
@@ -39,14 +52,58 @@ for xml in xmllist:
 	for p in root.iter('p'):
 		if bool(p.text):
 		###scan the paragraph with re for possible citations
-			doknrlist = doknrlist + scan_paragraph(p.text)
+			list_of_citations = scan_paragraph(p.text)
+			for quote in list_of_citations:
+				found_source = find_source(quote)
+				if(found_source):
+					doknrlist.append(found_source) 
 			
 		###add citations with doknrlist.append(citations)	
 	# listcount gibt immer die Anzahl an gefundenen Zitaten zurück (und löscht dann diese Einträge), wenn nichts gefunden wird, dann gibt es 0 zurück.
-	NStZ = listcount(doknrlist, 'NStZ')
-	Palandt = listcount(doknrlist, 'Palandt')
-	...
-	writer.writerow([doknr, aktenzeichen, gertyp, spruchkoerper, entschdatum, 'Zitat(Kommentar)', 'Zitat(Entscheidung)', 'Zitat(Autor)'])
+	BGH = listcount(doknrlist, 'BGH')
+	BVerfG = listcount(doknrlist, 'BVerfG')
+	LG_Hamburg = listcount(doknrlist, 'LG Hamburg')
+	AG_Hamburg = listcount(doknrlist, 'AG Hamburg')
+	OLG_Hamm = listcount(doknrlist, 'OLG Hamm')
+	OLG_München = listcount(doknrlist, 'OLG München')
+	OLG_Stuttgart = listcount(doknrlist, 'OLG Stuttgart')
+	OLG_Frankfurt = listcount(doknrlist, 'OLG Frankfurt')
+	OLG_Karlsruhe = listcount(doknrlist, 'OLG Karlsruhe')
+	OLG_Dresden = listcount(doknrlist, 'OLG Dresden')
+	OLG_Köln = listcount(doknrlist, 'OLG Köln')
+	OLG_Celle = listcount(doknrlist, 'OLG Celle')
+	OLG_Berlin = listcount(doknrlist, 'OLG Berlin')
+	OLG_Nürnberg = listcount(doknrlist, 'OLG Nürnberg')
+	OLG_Schleswig = listcount(doknrlist, 'OLG Schleswig')
+	OLG_Brandenburg = listcount(doknrlist, 'OLG Brandenburg')
+	OLG_Oldenburg = listcount(doknrlist, 'OLG Oldenburg')
+	OLG_Naumburg = listcount(doknrlist, 'OLG Naumburg')
+	OLG_Bamberg = listcount(doknrlist, 'OLG Bamberg')
+	OLG_Jena = listcount(doknrlist, 'OLG Jena')
+	OLG_Hamburg = listcount(doknrlist, 'OLG Hamburg')
+	OLG_Rostock = listcount(doknrlist, 'OLG Rostock')
+	OLG_Zweibrücken = listcount(doknrlist, 'OLG Zweibrücken')
+	OLG_Braunschweig = listcount(doknrlist, 'OLG Braunschweig')
+	OLG_Saarbrücken = listcount(doknrlist, 'OLG Saarbrücken')
+	KG = listcount(doknrlist, 'KG')
+	OLG_Saarland = listcount(doknrlist, 'OLG Saarland')
+	MüKo-BGB = listcount(doknrlist, 'MüKo-BGB')
+
+
+
+	writer.writerow([doknr, aktenzeichen, gertyp, spruchkoerper, entschdatum, jahr, BGH, BVerfG, LG_Hamburg, AG_Hamburg, 
+		OLG_Hamm, OLG_München, OLG_Stuttgart,
+		OLG_Frankfurt, OLG_Düsseldorf, OLG_Karlsruhe, OLG_Dresden, OLG_Köln, OLG_Celle,
+		OLG_Berlin, OLG_Nürnberg, OLG_Schleswig, OLG_Koblenz, OLG_Brandenburg, OLG_Oldenburg,
+		OLG_Naumburg, OLG_Bamberg, OLG_Jena, OLG_Hamburg, OLG_Rostock, OLG_Zweibrücken,
+		OLG_Braunschweig, OLG_Saarbrücken, OLG_Bremen, KG, OLG_Saarland, MüKo-BGB,
+		'MüKo-ZPO', 'MüKo-GmbHG', 'MüKo-InsO', 'MüKo-StGB', 'MüKo-StPO', 'MüKo-HGB', 'MüKo-AktG',
+		'Sodan/Ziekow', 'Staudinger', 'Palandt', 'jurisPK-BGB', 'BeckOK-BGB', 'BeckOK-ZPO', 'BeckOK-StGB',
+		'BeckOK-Datenschutzrecht', 'BeckOK-StPO', 'BeckOK-BauGB', 'BeckOK-FamFG', 'BeckOK-HGB', 'BeckOK-WEG',
+		'BeckOK-UrhR', 'BeckOK-AuslR', 'BeckOK-GG', 'BeckOK-VVG', 'BeckOK-GBO', 'BeckOK-VwVfG',
+		'Meyer-Goßner', 'Fischer', 'Leipziger Kommentar', 'Schönke/Schröder', 'Thomas/Putzo', 'Musielak-ZPO'
+		'Zöller-ZPO', 'Baumbach-ZPO', 'Wieczorek-ZPO', 'Prütting-ZPO', 'Hk-ZPO', 'Stein/Jonas-ZPO',
+		'Saenger-ZPO', 'Zimmermann-ZPO', 'Musielak-FamG', 'Baumbach-GmbH', 'Baumbach-HGB', 'Hk-InsO'])
 	print('Sucess:' + str(xml))
 	count += 1
 	#prints out the percentage of files already parsed and included in xml
